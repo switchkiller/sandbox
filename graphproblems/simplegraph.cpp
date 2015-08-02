b5b67fed6f757ea043b5fa3f9c1a65ce177077b3
switchkiller <emailbarung@gmail.com>
2015-08-02 21:54:54 +0530

    Bug fixed

--- graphproblems/simplegraph.cpp
+++ graphproblems/simplegraph.cpp
@@ -14,11 +14,12 @@ int num;
 
 void create(){
   int i,max_edges,origin,destin;
-  printf("%d",num); //Number of vertices
+  printf("Number of vertices: %d\n",num); //Number of vertices
   max_edges=num*(num-1);
   for (int i=0;i<=max_edges;i++){
     cout<<i<<":";
     cin>>origin>>destin;
+    //cout<<origin<<"--"<<destin<<endl;
     if ((origin==-1) && (destin==-1))   //-1 -1 to quit
       break;
     if ((origin<0) || (origin>=num) || (destin<0) || (destin>=num))
@@ -35,7 +36,8 @@ void insert_edge(int insert, int destin){
   if (destin<0 || destin>=num)
     //destination doesnot exist
     return;
-  adj[insert][destin]=0;
+  adj[insert][destin]=1;
+  //cout<<insert<<"--"<<destin<<endl;
 }
 
 void del_edge(int ins, int des){
@@ -49,9 +51,24 @@ void del_edge(int ins, int des){
 
 void display(){
   int i,j;
-  for(i=0;i<num;i++){
-    for(j=0;j<i;j++)
-      cout<<adj[i][j];
-    cout<<endl;
+  for(i=0;i<MAX;i++){
+    for(j=0;j<MAX;j++){
+        if (adj[i][j]==1)
+          cout<<i<<"--"<<j<<endl;
+      }
   }
+}
+
+
+int main(int argc, char *argv[]){
+  int x,y;
+  cin>>num;   //No. of vertices
+  create();
+  cin>>x>>y; //Node to insert
+  insert_edge(x,y);
+  cin>>x>>y; //Node to delete
+  del_edge(x,y);
+  cout<<"Graph Display: \n";
+  display();
+  return 0;
 }

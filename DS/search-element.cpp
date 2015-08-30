96f2ca40a2a53ae9672bff48e904c3ef4a008ccb
switchkiller <emailbarung@gmail.com>
2015-08-30 19:12:52 +0530

    Added recursive solution

--- DS/search-element.cpp
+++ DS/search-element.cpp
@@ -17,26 +17,37 @@ void push(struct node** head_ref, int data){
   (*head_ref) = current;
 }
 
-void search(struct node* head, int key){
+//Iterative Solution
+int searchItr(struct node* head, int key){
   struct node* temp;
   int index = 0;
   temp = head;
   while (temp != NULL){
     if (temp -> data == key)
-      cout<<index+1;
+      return index+1;
     index++;
     temp = temp -> next;
   }
 }
 
+// Recursive Solution
+int searchRec(struct node* head, int key, int plus){
+  if (head == NULL )
+    return -plus;
+  if (head ->data == key)
+    return 1;
+  return 1 + searchRec(head -> next, key, plus+1);
+}
+
 int main(){
   struct node* head = NULL;
-  int key = 10;
+  int key = 00;
   push(&head,10);
   push(&head,20);
   push(&head,30);
   push(&head,40);
   push(&head,50);
-  search(head,key);
+  cout<<searchItr(head,key);
+  cout<<searchRec(head,key,0);
   return 0;
 }

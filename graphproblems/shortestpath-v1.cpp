d598fdea5991e6f0553ba9216ebf0e69dadde1f8
switchkiller <emailbarung@gmail.com>
2015-09-13 16:15:17 +0530

    Changes continue

--- graphproblems/shortestpath-v1.cpp
+++ graphproblems/shortestpath-v1.cpp
@@ -22,3 +22,36 @@ int queue[MAXV], front = -1, rear = -1;
 void insert_queue(int v);
 int delete_queue();
 int isEmpty_queue();
+
+int main(){
+  int v, count =0,
+  create_graph();
+  BF_Traversal();
+  while(1){
+    cout << "Enter the destination vertex (-1 to quit.)";
+    cin >> v;
+    if (v < -1 || v > n-1){
+      cout<<"Does not exist.";
+      continue;
+    }
+    if (v == -1)
+      break;
+    if (distance[v] == infinity){
+      cout << "No path.";
+      continue;
+    }
+    else
+      cout << "Shortest Distance = " << distance[v];
+    while (v != NIL){
+      count++;
+      path[count] = v;
+      u = predecessor[v];
+      v = u;
+    }
+    cout << "Shortest path : ";
+    for (i = count; i > 1; i--)
+      cout << "->" << path[i];
+    cout << path[i];
+    cout << endl;
+  }
+}

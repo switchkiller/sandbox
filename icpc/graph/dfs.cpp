7b8cd1750ae573f51623986768a382058e83c2e1
switchkiller <emailbarung@gmail.com>
2015-09-19 17:36:28 +0530

    Checking the graph

--- icpc/graph/dfs.cpp
+++ icpc/graph/dfs.cpp
@@ -9,11 +9,12 @@ typedef vector<int> vi;
 
 #define DFS_WHITE -1
 #define DFS_BLACK 1
-
+#define DFS_GRAY 0
 vector<vii> AdjList;
 
 vi dfs_num;
 int numCC;
+vi dfs_parent;
 
 void dfs(int u){
   printf("%d",u);
@@ -31,5 +32,26 @@ void floodfill(int u, int color){
     ii v = AdjList[u][j];
     if (dfs_num[v.first] == DFS_WHITE)
       floodfill(v.first, color);
+  }
+}
+
+void graphCheck(int u){
+  dfs_num[u] = DFS_GRAY;
+  for (int j = 0; j < (int) AdjList[u].size(); j++){
+    ii v  = AdjList[u][j];
+    if (dfs_num[v.first] == DFS_WHITE){
+      dfs_parent[v.first] = u;
+      graphCheck(v.first);
+    }
+    else if(dfs_num[v.first] == DFS_GRAY){
+      if (v.first == dfs_parent[u])
+        printf("Bidirectional (%d, %d) - (%d, %d)\n", u, v.first, v.first, u);
+      else
+        printf("Back Edge (%d, %d) (Cycle)\n", u, v.first);
+      }
+      elseif(dfs_num[v.first] == DFS_BLACK)
+        printf("Forward/Cross Edge (%d, %d)\n", u, v.first);
+    }
+    dfs_num[u] = DFS_BLACK;
   }
 }

1933ee4e64cfef6bc987c6dbf4c916040a7ab846
switchkiller <emailbarung@gmail.com>
2015-10-25 08:36:56 +0530

    Fixed all errors :D

--- icpc/graph/uva/352.cpp
+++ icpc/graph/uva/352.cpp
@@ -1,17 +1,22 @@
 // Problem Statement - UVa 352 Seasonal War
 #include<iostream>
+#include<cstdio>
+#include<stdio.h>
+using namespace std;
 #define MAXN 30
-int a[MAXN][MAXN];
+char a[MAXN][MAXN];
+int n;
 int DFS(int i, int j){
- if (i < 0 || i > n || j < 0 || j > n) return; 
+ if (i < 0 || i >= n || j < 0 || j >= n || a[i][j] != '1') return 0;
  a[i][j] = '2';
  DFS(i-1,j-1); DFS(i-1, j); DFS(i-1, j+1);
  DFS(i, j-1); DFS(i, j+1);
  DFS(i+1, j-1); DFS(i+1, j); DFS(i+1, j+1);
 }
 int main(){
- int n, count = 0, k=0;
- while(cin>>n){
+ int count = 0, k=0;
+ while(cin >> n){
+ getchar();
  for (int i = 0; i < n; i++)
 	 gets(a[i]);
  for (int i = 0; i < n; i++)
@@ -20,6 +25,6 @@ int main(){
 		 	count++;
 			DFS(i,j);
 		 }
- }
  printf("Image number %d contains %d war eagles.\n", ++k, count);
+ }
 }

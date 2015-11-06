23a1f5a8ab0ffabd208e8f30aa188ff70ced0386
switchkiller <emailbarung@gmail.com>
2015-11-06 16:09:06 +0530

    785 Solved using flooddill

--- icpc/graph/uva/785.cpp
+++ icpc/graph/uva/785.cpp
@@ -4,80 +4,32 @@ using namespace std;
 char matrix[30+5][80+5];
 bool visited[30+5][80+5];
 int line;
-int dx[] = {-1,-1,-1,0,0,1,1,1}, dy[] = {-1,0,1,1,1,0,-1,-1};
-int a;
-void setup(){
-  REP(i,0,35){
-    REP(j,0,84){
-      if (matrix[i][j] == '#') matrix[i][j] = '1';
-      else if (matrix[i][j] == '/') matrix[i][j] = '2';
-      if (matrix[i][j] != 'X' && matrix[i][j] != '1' && matrix[i][j] != '2' && matrix[i][j] != '_')
-        matrix[i][j] = ' ';
-    }
-    matrix[i][84] = '\0';
-  }
-}
+int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+char mark;
 void dfs(int x, int y){
-  if (x < 0 || y < 0 || x == line || matrix[x][y] == '\0' || matrix[x][y] == 'X' || visited[x][y]) return;
-  visited[x][y] = true;
-  if (a == 1) matrix[x][y] = '#';
-  else if (a == 2) matrix[x][y] = '/';
-  REP(i,0,8)
+  if (matrix[x][y] == mark || matrix[x][y] == 'X') return;
+  matrix[x][y] = mark;
+  REP(i,0,4)
         dfs(x + dx[i],y + dy[i]);
 }
-void reverseSetup() {
-    for ( int i = 34; i >= 0; i-- ) {
-        bool foundLine = false;
-        for ( int j = 83; j >= 0; j-- ) {
-            if (matrix [i] [j] == 'X' || matrix [i] [j] == '#' || matrix[i][j] == '/' || matrix [i] [j] == '_') {
-                matrix [i] [j + 1] = '\0';
-                foundLine = true;
-                break;
-            }
-        }
-        if (!foundLine) matrix [i] [0] = '\0';
-    }
-}
-void cleanup() {
-    for ( int i = 0; i < 35; i++ ) {
-        for ( int j = 0; j < 85; j++ ) {
-            matrix [i] [j] = '\0';
-        }
-    }
-}
 int main(){
-  int testcases;
-  scanf ("%d", &testcases);
-  getchar();
-  while (testcases--){
     while(gets(matrix[0])){
       line = 1;
       while (gets(matrix[line])){
         if (matrix[line][0] == '_') break;
         line++;
       }
-      setup();
       REP(i,0,line){
         REP(j,0,strlen(matrix[i])){
-          if (matrix[i][j] == '1'){
-            memset(visited,false,sizeof visited);
-            a = 1;
-            matrix[i][j] = ' ';
-            dfs(i,j);
-          }
-          else if (matrix[i][j] == '2'){
-            memset(visited,false,sizeof visited);
-            a = 2;
+          if (matrix[i][j] != ' ' && matrix[i][j] != 'X'){
+            mark = matrix[i][j];
             matrix[i][j] = ' ';
             dfs(i,j);
           }
         }
       }
-      reverseSetup();
       REP(i,0,line+1)
         cout << matrix[i] << endl;
-      }
-      cleanup();
     }
   return 0;
 }

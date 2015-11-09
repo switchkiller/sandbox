251018d2f88e19c3c322b70ea4552163822ca3b8
switchkiller <emailbarung@gmail.com>
2015-11-09 14:41:01 +0530

    Art vertex and bridges - general

--- icpc/graph/articulation_points.cpp
+++ icpc/graph/articulation_points.cpp
@@ -15,6 +15,25 @@ vi dfs_parent;
 vi art_ver;
 int dfsNum, dfsRoot, rootChild;
 
+void articulationPointAndBridge(int u){
+    dfs_low[u] = dfs_num[u] = dfsNum++;
+    REP(j,0,AdjList[u].size()){
+      ii v = AdjList[u][j];
+      if (dfs_num[v.first] == DFS_WHITE){
+        dfs_parent[v.first] = u;
+        if (u == dfsRoot) rootChild++;
+        articulationPointAndBridge(v.first);  // XXX after you start backtracking
+        if (dfs_low[v.first] >= dfs_num[u])
+          art_ver[u] = true;
+        if (dsf_low[v.first] > dfs_num[u])
+          cout << u << "-" << v.first << " is a bridge.";
+        dfs_low[u] = min(dfs_low[u],dfs_low[v.first]); //Backedge the ancestor will get updated automatically as a result of backtrack
+      }
+      else if (v.first != dfs_parent[u])
+        dfs_low[u] = min(dfs_low[u],dfs_num[v.first]);
+    }
+}
+
 int main(){
   int V, neighbour, id, weight;
   cin >> V;
@@ -30,4 +49,21 @@ int main(){
   dfs_low.assign(V,0);
   dfs_parent.assign(V,-1);
   art_ver.assign(V,0);
+
+  cout << "Bridges: ";
+  REP(i,0,V){
+    if (dfs_num[i] == DFS_WHITE){
+      dfsRoot = i; rootChild = 0;
+      articulationPointAndBridge(i);
+      art_ver[dfsRoot] == (rootChild > 1);
+    }
+  }
+
+  cout << "Articulation Vertex: ";
+  REP(i,0,V){
+    if (art_ver[i]){
+      cout << i;
+    }
+  }
+  return 0;
 }

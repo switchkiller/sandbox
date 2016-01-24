1a0d7373be30b81a787bfd3ea2919468439128a2
switchkiller <emailbarung@gmail.com>
2016-01-24 18:44:47 +0530

    Use Virtual Functions

---
+++ contest/hackerrank/code-cpp/overload-op.cpp
@@ -0,0 +1,58 @@
+XXX : TODO
+XXX : Operator overloading
+#include<iostream>
+using namespace std;
+
+class Complex
+{
+public:
+    int a,b;
+    void input(string s)
+    {
+        int v1=0;
+        int i=0;
+        while(s[i]!='+')
+        {
+            v1=v1*10+s[i]-'0';
+            i++;
+        }
+        while(s[i]==' ' || s[i]=='+'||s[i]=='i')
+        {
+            i++;
+        }
+        int v2=0;
+        while(i<s.length())
+        {
+            v2=v2*10+s[i]-'0';
+            i++;
+        }
+        a=v1;
+        b=v2;
+    }
+};
+
+//Overload operators + and << for the class complex
+//+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
+//<< should print a complex number in the format "a+ib"
+class complex: public Complex{
+public:
+  Complex operator+(const Complex& other);
+  //friend ostream &operator<<(ostream &out, complex c);
+};
+Complex Complex::operator+(const Complex& z){
+      Complex comp;
+      comp.a = this->a + num2.a;
+      comp.b = this->b + num2.b;
+      return comp;
+}
+int main()
+{
+    Complex x,y;
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+    x.input(s1);
+    y.input(s2);
+    Complex z=x+y;
+    //cout<<z<<endl;
+}
---
+++ contest/hackerrank/code-cpp/virtual-class.cpp
@@ -0,0 +1,83 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 100
+
+class Person{
+public:
+  char name[20];
+  int age;
+  virtual void getdata(){  };
+  virtual void putdata(){  };
+};
+class Professor: public Person{
+public:
+  int publications;
+  static int current_id;
+  int val;
+  void getdata(){
+    cin >> name;
+    cin >> age;
+    cin >> publications;
+    current_id += 1;
+    val = current_id;
+  }
+  void putdata(){
+    cout << name << " " << age << " " << publications << " " << val << endl;
+  }
+};
+class Student: public Person{
+public:
+  int mark[6];
+  int sum;
+  int val;
+  static int current_id;
+
+  void getdata(){
+      sum = 0;
+      cin >> name;
+      cin >> age;
+      for (int i = 0; i < 6; i++){
+        cin >> mark[i];
+        sum += mark[i];
+      }
+      current_id += 1;
+      val = current_id;
+  }
+  void putdata(){
+    cout << name << " " << sum << " " << val << endl;
+  }
+};
+int Professor::current_id = 0;
+int Student::current_id = 0;
+
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}

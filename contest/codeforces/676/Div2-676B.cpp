573bdabf6e0f93c657d2cf1da03418c2a3b14132
switchkiller <emailbarung@gmail.com>
2016-05-26 01:47:05 +0545

    Tricky

--- contest/codeforces/676/Div2-676B.cpp
+++ contest/codeforces/676/Div2-676B.cpp
@@ -1,27 +1,18 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-
-#define REP(i,a,b) for (int i = (a); i < (b); i++)
-
+#define REP(i,a,b) for (int i = (a); i <= (b); i++)
 int main(){
-	int n, t;
+	double w[11][11];
+	int n,t,res=0,i,j;
 	cin >> n >> t;
-	map<int, int> mp;
-	int cnt=1, val=1, d=t, res=0, mod=0;
-	mp[0] = t;
-	while (cnt <= n){
-		REP(i,0,cnt){
-				mp[mod] += d;
-				cout << val << " " << d << endl;
-				if (d > 0) res++;
-				val++;
+	w[1][1]=t;
+	REP(i,1,n)
+		REP(j,1,i)
+			if(w[i][j]>=1){
+				w[i+1][j]+=(w[i][j]-1)/2;
+				w[i+1][j+1]+=(w[i][j]-1)/2;
+				res++;
 			}
-
-		}
-		d = (d-1)/2;
-		cnt++; mod++;
-	}
 	cout << res;
 	return 0;
 }

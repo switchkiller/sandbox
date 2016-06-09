88aca42010729e467e2079d7287f432e70dc22f3
switchkiller <emailbarung@gmail.com>
2016-06-09 19:58:16 +0545

    Recursive soln

---
+++ contest/codeforces/680/Div2/C3.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+inline ll sq(ll n)
+{
+    return n*n*n;
+}
+ pair<ll,ll> best;
+void rec(ll m,ll steps,ll sum)
+{
+    if(m==0)
+    {
+       best=max(best,make_pair(steps,sum));
+       return;
+    }
+    else
+    {
+        ll x=1;
+        while(sq(x+1)<=m) x++;
+        rec(m-sq(x),steps+1,sum+sq(x));
+        if(x>=1)
+        {
+            rec(sq(x)-1-sq(x-1),steps+1,sum+=sq(x-1));
+        }
+    }
+}
+int main(){
+
+    ll m;
+    cin>>m;
+    rec(m,0,0);
+    cout<<best.first<<" "<<best.second<<endl;
+	return 0;
+}

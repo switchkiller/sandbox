dcb3749f895e94acf8d740dfab044498f7b970c0
switchkiller <emailbarung@gmail.com>
2016-07-02 00:09:38 +0545

    Little changes to class #dp

--- topcoder/DP/BadNeighbors.cpp
+++ topcoder/DP/BadNeighbors.cpp
@@ -19,25 +19,25 @@ typedef vector<ii> vii;
 
 class BadNeighbors{
   public:
-      int maxDonations(vector <int> donations){
-          n = donations.size();
+      int maxdonations(vector <int> donations){
+          int n = donations.size();
           int dp[N] = {0};
           bool zero[N], idx[N];
-          dp[0] = a[0];
+          dp[0] = donations[0];
           idx[0] = zero[0] = true;
 
-          if (a[1] > dp[0]){
-              dp[1] = a[1];
+          if (donations[1] > dp[0]){
+              dp[1] = donations[1];
               idx[0] = true;
           }
           else{
               dp[1] = dp[0];
               zero[1] = true;
           }
-          // Note that dp[i-2] is sum before a[i-1] actually occured. This plays an important role.
+          // Note that dp[i-2] is sum before donations[i-1] actually occured. This plays an important role.
           REP(i,2,n){
-              if (dp[i-2] + a[i] > dp[i-1]){
-                  dp[i] = dp[i-2] + a[i];
+              if (dp[i-2] + donations[i] > dp[i-1]){
+                  dp[i] = dp[i-2] + donations[i];
                   idx[i] = true;
                   zero[i] = zero[i-2];
               }
@@ -48,10 +48,7 @@ class BadNeighbors{
           }
           // If first and last are both contributing, take maximum of who contributed.
           if (zero[n-1] && idx[n-1])
-              cout << max(dp[n-2], max(dp[n-1]-a[n-1], dp[n-1]-a[0]));
-          else   cout << dp[n-1];
-
-
+              return max(dp[n-2], max(dp[n-1]-donations[n-1], dp[n-1]-donations[0]));
+          else   return dp[n-1];
       }
-
 };

e32eafb44661c10eaa7f69ddfd247c92c108636d
switchkiller <emailbarung@gmail.com>
2016-07-25 16:03:42 +0530

    optimized. working

--- miscellaneous/implementation/Gayle/Trees-and-Graphs/45.c
+++ miscellaneous/implementation/Gayle/Trees-and-Graphs/45.c
@@ -4,59 +4,47 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
 struct node {
-  int data;
-  struct node *left, *right;
+    int data;
+    struct node *left, *right;
 };
 
+int i = 0;
 struct node *newNode(int data){
-  struct node *tmp = (struct node*) malloc(sizeof(struct node));
-  tmp->data = data;
-  tmp->left = NULL;
-  tmp->right = NULL;
-  return tmp;
+        struct node *temp = (struct node *) malloc(sizeof (struct node));
+        temp->data = data;
+        temp->left = temp->right = NULL;
+        return temp;
 }
 
-void inorder(struct node *root){
-  if (root == NULL) return;
-
-  inorder(root->left);
-  printf("%d\n",root->data);
-  inorder(root->right);
+bool btisbst(struct node *root){
+    if (root == NULL) return true;
+    if (!btisbst(root->left))
+        return false;
+    if ( i != 0 && root->data <= i)
+        return false;
+    i = root->data;
+    if (!btisbst(root->right))
+        return false;
+    return true;
 }
 
-static int i = 0;
-
-bool isbst(struct node *root){
-  if (root = NULL) return true;
-
-  if (!isbst(root->left))
-    return false;
-
-  if (i != 0  && root->data <= i){
-    return false;
-  }
-  i = root->data;
-
-  if (!isbst(root->right))
-    return false;
-
-  return true;
-}
 
 int main(){
-  struct node *root = NULL;
-  root = newNode(50);
-  root->left = newNode(30);
-  root->left->left = newNode(20);
-  root->left->right = newNode(40);
-  root->right = newNode(70);
-  root->right->left = newNode(60);
-  root->right->right = newNode(80);
+    node *root = newNode(50);
+    root->left = newNode(30);
+    root->left->left = newNode(20);
+    root->left->right = newNode(40);
+    root->right = newNode(70);
+    root->right->left = newNode(60);
+    root->right->right = newNode(80);
 
-  // inorder(root);
-  printf("%d\n",isbst(root) );
+    cout << btisbst(root);
+    return 0;
+}
 
-
-  return 0;
 }

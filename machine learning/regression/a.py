9e94e202e3c6b109b55132b8a10bf29ef7ecd34d
switchkiller <emailbarung@gmail.com>
2016-10-16 09:22:46 +0530

    Generating graph stock

--- machine learning/regression/a.py
+++ machine learning/regression/a.py
@@ -1,9 +1,14 @@
 #Linear regression on google stock
+#!/usr/bin/env python3.5
 import pandas as pd
-import quandl, math
+import quandl, math, datetime, time
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -23,15 +28,47 @@ df.fillna(-99999, inplace=True)
 forecast_out = int(math.ceil(0.01*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
-# print(df.head())
+print(forecast_out)
 
 #here we define X features and y labels
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
-
 #normalize the data. Basically it will reduce the computation time
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
 
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+
+#check if everything is okay...tada
 print(len(X), len(y))
+
+X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, Y_train)
+accuracy = clf.score(X_test, Y_test)
+#print (accuracy)
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.NaN
+last_date = df.iloc[-1].name
+last_unix = time.mktime(last_date.timetuple())
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()

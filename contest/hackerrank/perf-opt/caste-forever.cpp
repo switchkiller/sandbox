5632ec167432adf927554021a023a1e05d212f9a
Barun Singhania <emailbarung@gmail.com>
2017-10-06 11:37:37 +0530

    Some competition

---
+++ contest/hackerrank/perf-opt/caste-forever.cpp
@@ -0,0 +1,81 @@
+/* Problem Description:
+
+*/
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+#define REP(i,a,b) for (int i = (a); i < (b); i++)
+#define RREP(i,a,b) for (int i = (a); i > (b); i--)
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define TRvi(it, c) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define inf 0x3f3f3f3f
+#define EPS 1e-10
+#define PI acos(-1.0)
+#define scan(x) scanf("%d",x)
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define LSOne(x) x&(-x)
+#define debug(x) cout << #x << " " << x << endl;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+   //  freopen("in" , "r" , stdin);
+   //  freopen("out" , "w" , stdout);
+    #endif
+    int n, cnt = 1;
+    cin >> n;
+    long arr[n], max1, max2;
+    REP(i,0,n) cin >> arr[i];
+    max1 = arr[0], max2 = -inf;
+    REP(i,1,n){
+      if (arr[i] > max1){ max1 = arr[i]; cnt = 1;}
+      else if (arr[i] >= max2 && max1 >= max2){
+        max2 = arr[i];
+        if (max1 == max2) cnt++;
+      }
+      // cout << cnt;
+    }
+    cout << cnt << endl;
+    return 0;
+ }

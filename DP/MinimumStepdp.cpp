a52873efef895c2147c4f3ca6ae95121eef487a9
Barun Singhania <emailbarung@gmail.com>
2019-02-24 18:58:21 +0530

    all changes

--- DP/MinimumStepdp.cpp
+++ DP/MinimumStepdp.cpp
@@ -1,25 +1,24 @@
 // Description - Minimum number of steps to divide a number upto 1 by perfoming some mathematical functions.
 // On a positive integer, you can perform any one of the following 3 steps. 1.) Subtract 1 from it. ( n = n - 1 )  , 2.) If its divisible by 2, divide by 2. ( if n % 2 == 0 , then n = n / 2  )  , 3.) If its divisible by 3, divide by 3. ( if n % 3 == 0 , then n = n / 3  ). Now the question is, given a positive integer n, find the minimum number of steps that takes n to 1.
-//BU approach
+//TD approach
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 100
 int lookup[MAX+1];
-int minimumSteps(int n){
-  lookup[1] = 0;
-  for (int i = 2; i <= n; i++){
-    lookup[i] = 1 + lookup[i-1];
-    if (i%2 == 0) lookup[i] = min(lookup[i], 1 + lookup[i/2]);
-    if (i%3 == 0) lookup[i] = min(lookup[i], 1 + lookup[i/3]);
-    cout << i << " " << lookup[i] << endl;
-  }
-  return lookup[n];
+int min_steps(int n){
+  if (n == 1) return 0;
+  if (lookup[n] != -1) return lookup[n];
+  int r = 1 + min_steps(n-1);
+  if (n % 2 == 0) r = min(r, 1 + min_steps(n/2));
+  if (n % 3 == 0) r = min(r, 1 + min_steps(n/3));
+  cout << n << "=" << r << endl;
+  lookup[n] = r; // XXX min val
+  return r;
 }
-
 int main(){
   int n;
   memset(lookup, -1, sizeof lookup);
   cin >> n;
-  cout << minimumSteps(n);
+  cout << min_steps(n);
   return 0;
 }

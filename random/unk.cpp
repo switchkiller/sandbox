442426cf2de08c400fc4d9f6f2b7c71af2816aee
Barun Singhania <emailbarung@gmail.com>
2019-05-01 01:24:51 +0530

    simple prob

---
+++ random/unk.cpp
@@ -0,0 +1,84 @@
+/* Problem Description:
+
+*/
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+#define REP(i,a,b) for (int i = (a); i < (b); i++)
+#define RREP(i,a,b) for (int i = (a); i > (b); i--)
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define TRvi(it, c) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define inf 0x3f3f3f3f
+#define EPS 1e-10
+#define PI acos(-1.0)
+#define scan(x) scanf("%d",x)
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define LSOne(x) x&(-x)
+#define debug(x) cout << #x << " " << x << endl;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+int solve(int l, int r, int mid, int k){
+  if (l < r){
+    int pos = l + (r-l)/2;
+    if (pos == k) return mid;
+    if (pos > k) return solve(l, pos-1, mid-1, k);
+    return solve(pos+1, r, mid-1, k);
+  }
+  return 0;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+   //  freopen("in" , "r" , stdin);
+   //  freopen("out" , "w" , stdout);
+    #endif
+    int m,n,k;
+    cin >> m >> n >> k;
+    int ans = solve(1,pow(2,n+1),m+n,k);
+    if (ans == 0)
+      cout << m << endl;
+    else
+      cout << ans << endl;
+    return 0;
+ }

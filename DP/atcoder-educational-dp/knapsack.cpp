bfba318314b395d0cc8ec665741986186ca346c5
Barun Singhania <emailbarung@gmail.com>
2019-08-19 21:40:07 +0530

    atcoder edu dp

---
+++ DP/atcoder-educational-dp/knapsack.cpp
@@ -0,0 +1,89 @@
+/* Problem Description:
+
+*/
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+#define REP(i,a,b) for (int i = (a); i < (b); i++)
+#define RREP(i,a,b) for (int i = (a); i > (b); i--)
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define TRvi(it, c) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define inf 0x3f3f3f3f
+#define EPS 1e-10
+#define PI acos(-1.0)
+#define scan(x) scanf("%d",x)
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define LSOne(x) x&(-x)
+#define debug(x) cout << #x << " " << x << endl;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+   //  freopen("in" , "r" , stdin);
+   //  freopen("out" , "w" , stdout);
+    #endif
+    int n, w;
+    cin >> n >> w;
+    int weight[n+1]; long value[n+1];
+
+    for (int i = 1; i <= n; i++){
+      cin >> weight[i] >> value[i];
+    }
+
+    vector< vector<long> > dp(w+1, vector<long>(n+1, 0));
+
+    for (int i = 1; i <= w; i++){
+      for (int j = 1; j <= n; j++){
+        if (i-weight[j] >= 0){
+          dp[i][j] = max(dp[i][j-1], value[j] + dp[i-weight[j]][j-1]);
+        }
+        else
+        dp[i][j] = dp[i][j-1];
+      }
+    }
+
+    cout << dp[w][n] << endl;
+
+    return 0;
+ }
---
+++ DP/atcoder-educational-dp/vacation.cpp
@@ -0,0 +1,83 @@
+/* Problem Description:
+
+*/
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+#define REP(i,a,b) for (int i = (a); i < (b); i++)
+#define RREP(i,a,b) for (int i = (a); i > (b); i--)
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define TRvi(it, c) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define inf 0x3f3f3f3f
+#define EPS 1e-10
+#define PI acos(-1.0)
+#define scan(x) scanf("%d",x)
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define LSOne(x) x&(-x)
+#define debug(x) cout << #x << " " << x << endl;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+   //  freopen("in" , "r" , stdin);
+   //  freopen("out" , "w" , stdout);
+    #endif
+    int n;
+    cin >> n;
+    int val;
+    vector< vector<int> > dp(n+1, vector<int> (3, -inf));
+    dp[0][0] = 0, dp[0][1] = 0, dp[0][2] = 0;
+    for (int i = 1; i <= n; i++){
+      for (int j = 0; j < 3; j++){
+        cin >> val;
+        for (int k = 0; k < 3; k++){
+          if (j != k)
+            dp[i][j] = max(dp[i][j], val + dp[i-1][k]);
+        }
+      }
+    }
+
+    cout << max(max(dp[n][0],dp[n][1]), dp[n][2]);
+    return 0;
+ }

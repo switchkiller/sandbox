df1e8fddbaa5ab12ceb99572c0adbb5959cb1e2e
Barun Singhania <emailbarung@gmail.com>
2019-11-12 19:43:23 +0530

    warmup

---
+++ gym/pushup/MinimumInSortedRotatedArray.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+#define REP(i,a,b) for (int i = (a); i < (b); i++)
+#define RREP(i,a,b) for (int i = (a); i > (b); i--)
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define TRvi(it, c) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define inf 0x3f3f3f3f
+#define EPS 1e-10
+#define PI acos(-1.0)
+#define scan(x) scanf("%d",x)
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define LSOne(x) x&(-x)
+#define debug(x) cout << #x << " " << x << endl;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+int findMin(int arr[], int low, int high){
+  if (low <= high){
+    int mid = (low+high)/2;
+    if ((mid == low || arr[mid] < arr[mid-1]) && (mid == high || arr[mid] < arr[mid+1]) )return mid;
+    if (arr[high] > arr[mid])
+      return findMin(arr,low,mid-1);
+    return findMin(arr,mid+1,high);
+  }
+  return -1;
+}
+
+
+
+int main(){
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i = 0; i < n; i++)
+    cin >> arr[i];
+  int res = findMin(arr,0,n-1);
+
+  cout << res;
+
+  return 0;
+}
